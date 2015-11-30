5544ee7c596a8d6343e854b83612f869218f929e
Alex White <awhit012@gmail.com>
2015-11-30 11:28:36 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var app = angular.module("ngFun", []);
+
+app.controller("PokemonCtrl", function($scope) {
+    $scope.pokemon = [
+    {
+      Ndex: 25,
+      name: 'Pikachu',
+      type: 'Electric'
+    },
+    {
+      Ndex: 10,
+      name: 'Caterpie',
+      type: 'Bug'
+    },
+    {
+      Ndex: 39,
+      name: 'Jigglypuff',
+      type: 'Fairy'
+    },
+    {
+      Ndex: 94,
+       name: 'Gengar',
+      type: 'Ghost'
+    },
+    {
+      Ndex: 143,
+      name: 'Snorlax',
+      type: 'Normal'
+    }
+  ];
+  $scope.catchphrase = "Gotta catch 'em all!"
+});
